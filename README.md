5466d3f4525f0aaa905ad1b66d50a6c83b0a6895
Charles Abeling <charlieabeling@gmail.com>
2015-09-24 17:38:55 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# CFDispatcher
+
+rip off of mrdoobs dispatcher
+
+## Usage
+
+```javascript
+
+var yourFunction = function(){};
+
+CFDispatcher.prototype.apply( yourFunction.prototype );
+
+```
+
+Also available in node
---
+++ src/CFDispatcher.js
@@ -0,0 +1,114 @@
+/**
+ * based on mrdoob's named like Node's EventEmitter
+ * Used primarily as an inheritance via apply
+ *
+ * @author mrdoob <mrdoob@mrdoob.com>, chuckfairy <chuck@chuckfairy.com>
+ *
+ */
+
+function CFEventDispatcher() {}
+
+CFEventDispatcher.prototype = {
+
+	constructor: CFEventDispatcher,
+
+	apply: function ( object ) {
+
+		object.on = CFEventDispatcher.prototype.on;
+		object.hasListener = CFEventDispatcher.prototype.hasListener;
+		object.removeListener = CFEventDispatcher.prototype.removeListener;
+		object.dispatch = CFEventDispatcher.prototype.dispatch;
+
+        return object;
+
+	},
+
+	on: function ( type, listener ) {
+
+		if ( this._listeners === undefined ) this._listeners = {};
+
+		var listeners = this._listeners;
+
+		if ( listeners[ type ] === undefined ) {
+
+			listeners[ type ] = [];
+
+		}
+
+		if ( listeners[ type ].indexOf( listener ) === - 1 ) {
+
+			listeners[ type ].push( listener );
+
+		}
+
+	},
+
+	hasListener: function ( type, listener ) {
+
+		if ( this._listeners === undefined ) return false;
+
+		var listeners = this._listeners;
+
+		if ( listeners[ type ] !== undefined && listeners[ type ].indexOf( listener ) !== - 1 ) {
+
+			return true;
+
+		}
+
+		return false;
+
+	},
+
+	removeListener: function ( type, listener ) {
+
+		if ( this._listeners === undefined ) return;
+
+		var listeners = this._listeners;
+		var listenerArray = listeners[ type ];
+
+		if ( listenerArray !== undefined ) {
+
+			var index = listenerArray.indexOf( listener );
+
+			if ( index !== - 1 ) {
+
+				listenerArray.splice( index, 1 );
+
+			}
+
+		}
+
+	},
+
+	dispatch: function ( event ) {
+
+		if ( this._listeners === undefined ) return;
+
+		var listeners = this._listeners;
+		var listenerArray = listeners[ event.type ];
+
+		if ( listenerArray !== undefined ) {
+
+			event.target = this;
+
+			var array = [];
+			var length = listenerArray.length;
+
+			for ( var i = 0; i < length; i++ ) {
+
+				array[ i ] = listenerArray[ i ];
+
+			}
+
+			for ( var i = 0; i < length; i++ ) {
+
+				array[ i ].call( this, event );
+
+			}
+
+		}
+
+	}
+
+};
+
